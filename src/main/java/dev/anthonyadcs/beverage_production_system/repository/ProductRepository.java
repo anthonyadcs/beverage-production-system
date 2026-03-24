845edcf0b2a32e4cdbb21289e7cd6964ec080979
Anthony <anthonydacsilva10@gmail.com>
2026-03-24 02:53:55 -0300

    feat(product/repository): add JPA Specification support to ProductRepository

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/ProductRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/ProductRepository.java
@@ -3,9 +3,10 @@ package dev.anthonyadcs.beverage_production_system.repository;
 import dev.anthonyadcs.beverage_production_system.domain.entity.Product;
 import dev.anthonyadcs.beverage_production_system.domain.valueObject.EntityCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     boolean existsByCode(EntityCode code);
 }
