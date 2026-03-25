418c50e2110dfb63dce4c46bafd56dd1791472f1
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 19:59:43 -0300

    feat(stock-movement): implement StockMovement repository

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/StockMovementRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/StockMovementRepository.java
@@ -1,4 +1,9 @@
 package dev.anthonyadcs.beverage_production_system.repository;
 
-public interface StockMovementRepository {
+import dev.anthonyadcs.beverage_production_system.domain.entity.StockMovement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StockMovementRepository extends JpaRepository<StockMovement, UUID> {
 }
