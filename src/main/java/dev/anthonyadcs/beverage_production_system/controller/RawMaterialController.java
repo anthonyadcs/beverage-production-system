17bf9047e81c9a4b07671bfc9834d7112f3a7dc0
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 00:44:36 -0300

    feat(raw-material/controller): expose POST /raw-machines endpoint

--- src/main/java/dev/anthonyadcs/beverage_production_system/controller/RawMaterialController.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/controller/RawMaterialController.java
@@ -1,4 +1,25 @@
 package dev.anthonyadcs.beverage_production_system.controller;
 
+import dev.anthonyadcs.beverage_production_system.dto.request.CreateRawMaterialRequest;
+import dev.anthonyadcs.beverage_production_system.dto.response.RawMaterialResponse;
+import dev.anthonyadcs.beverage_production_system.service.RawMaterialService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/raw-materials")
 public class RawMaterialController {
+    @Autowired
+    private RawMaterialService rawMaterialService;
+
+    @PostMapping
+    public ResponseEntity<RawMaterialResponse> create(@RequestBody @Valid CreateRawMaterialRequest requestBody){
+        return ResponseEntity.status(HttpStatus.OK).body(rawMaterialService.create(requestBody));
+    }
 }
