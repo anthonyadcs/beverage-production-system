8699edd59c215a76d20d50d4de1f3ca1c9a6e05a
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 14:11:31 -0300

    feat(raw-material/dto): implement UpdateRawMaterialRequest

--- src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/UpdateRawMaterialRequest.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/UpdateRawMaterialRequest.java
@@ -1,4 +1,23 @@
 package dev.anthonyadcs.beverage_production_system.dto.request;
 
-public class UpdateRawMaterialRequest {
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record UpdateRawMaterialRequest(
+        @Size(min = 2, max = 100, message = "O nome do insumo deve ter entre 2 e 100 caracteres")
+        String name,
+
+        @Size(max = 500, message = "O números de caracteres máximo aceito para a descrição do insumo é 500.")
+        String description,
+
+        @PositiveOrZero
+        @DecimalMin(value = "0.000", inclusive = false)
+        BigDecimal minimumStock
+) {
+    public boolean isEmpty() {
+        return name == null && description == null && minimumStock == null;
+    }
 }
