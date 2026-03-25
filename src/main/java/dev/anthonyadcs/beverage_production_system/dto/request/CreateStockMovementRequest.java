814685f98a2ff19bcb008258f708e2d845caf8ba
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 20:03:07 -0300

    feat(stock-movement/dto): implement CreateStocMovementRequest and StockMovementResponse

--- src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/CreateStockMovementRequest.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/CreateStockMovementRequest.java
@@ -1,13 +1,23 @@
 package dev.anthonyadcs.beverage_production_system.dto.request;
 
 import dev.anthonyadcs.beverage_production_system.domain.enums.StockMovementType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record CreateStockMovementRequest (
         @NotNull(message = "O tipo de movimentação de estoque é necessário para sua criação.")
         StockMovementType type,
+
+        @NotNull(message = "A quantidade do insumo utilizada na movimentação de estoque é necessária para sua criação.")
+        @DecimalMin(value = "0.000", inclusive = false, message = "A quantidade do insumo utilizada deve ser maior que 0")
         BigDecimal movedQuantity,
-        String reason;
+
+        @NotBlank(message = "O motivo da movimentação de estoque é necessário para sua criação.")
+        String reason,
+
+        UUID productionOrderId
 ) {}
--- src/main/java/dev/anthonyadcs/beverage_production_system/dto/response/StockMovementResponse.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/dto/response/StockMovementResponse.java
@@ -1,4 +1,34 @@
 package dev.anthonyadcs.beverage_production_system.dto.response;
 
-public class StockMovementResponse {
+import dev.anthonyadcs.beverage_production_system.domain.entity.StockMovement;
+import dev.anthonyadcs.beverage_production_system.domain.enums.StockMovementType;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public record StockMovementResponse(
+        UUID id,
+        UUID rawMaterialId,
+        StockMovementType type,
+        BigDecimal movedQuantity,
+        BigDecimal previousStock,
+        BigDecimal resultingStock,
+        String reason,
+        //TODO: ADICIONAR "Production Order" ID NESTE RESPONSE QUANDO TIVER RELAÇÃO NA ENTIDADE
+        Instant createdAt
+) {
+    public static StockMovementResponse fromEntity(StockMovement stockMovement){
+        return new StockMovementResponse(
+                stockMovement.getId(),
+                stockMovement.getRawMaterial().getId(),
+                stockMovement.getType(),
+                stockMovement.getMovedQuantity(),
+                stockMovement.getPreviousStock(),
+                stockMovement.getResultingStock(),
+                stockMovement.getReason(),
+                stockMovement.getCreatedAt()
+        );
+    }
+
 }
