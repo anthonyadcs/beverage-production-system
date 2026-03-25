9c256dca53824ace14a2c6af1b88861f16e3ec47
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 02:59:04 -0300

    feat(raw-material/repository): add JpaSpecificatorExecutor to RawMaterialRepository

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/RawMaterialRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/RawMaterialRepository.java
@@ -3,9 +3,10 @@ package dev.anthonyadcs.beverage_production_system.repository;
 import dev.anthonyadcs.beverage_production_system.domain.entity.RawMaterial;
 import dev.anthonyadcs.beverage_production_system.domain.valueObject.EntityCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface RawMaterialRepository extends JpaRepository<RawMaterial, UUID> {
+public interface RawMaterialRepository extends JpaRepository<RawMaterial, UUID>, JpaSpecificationExecutor<RawMaterial> {
     boolean existsByCode(EntityCode code);
 }
