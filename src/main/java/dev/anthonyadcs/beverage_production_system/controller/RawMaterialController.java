cbbe3db210783abd6845f817b4b2f380ec0c98a0
Anthony <anthonydacsilva10@gmail.com>
2026-03-25 03:08:53 -0300

    feat(raw-material/controller): expose GET /raw-materials endpoint with query filtering and pagination

--- src/main/java/dev/anthonyadcs/beverage_production_system/controller/RawMaterialController.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/controller/RawMaterialController.java
@@ -1,16 +1,22 @@
 package dev.anthonyadcs.beverage_production_system.controller;
 
+import dev.anthonyadcs.beverage_production_system.domain.enums.RawMaterialUnitOfMeasure;
 import dev.anthonyadcs.beverage_production_system.dto.request.CreateRawMaterialRequest;
+import dev.anthonyadcs.beverage_production_system.dto.response.PageResponse;
 import dev.anthonyadcs.beverage_production_system.dto.response.RawMaterialResponse;
 import dev.anthonyadcs.beverage_production_system.service.RawMaterialService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/raw-materials")
@@ -19,7 +25,31 @@ public class RawMaterialController {
     private RawMaterialService rawMaterialService;
 
     @PostMapping
-    public ResponseEntity<RawMaterialResponse> create(@RequestBody @Valid CreateRawMaterialRequest requestBody){
+    public ResponseEntity<RawMaterialResponse> create(@RequestBody @Valid CreateRawMaterialRequest requestBody) {
         return ResponseEntity.status(HttpStatus.OK).body(rawMaterialService.create(requestBody));
+    }
+
+    @GetMapping
+    public PageResponse<RawMaterialResponse> listAll(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) List<String> unitOfMeasure,
+            @RequestParam(required = false, defaultValue = "false") Boolean lowStock,
+            @RequestParam(required = false, defaultValue = "true") List<Boolean> active,
+            @PageableDefault(size = 20) Pageable pageable
+    ) {
+        if(pageable.getSort().isUnsorted()){
+            pageable = PageRequest.of(
+                    pageable.getPageNumber(),
+                    pageable.getPageSize(),
+                    Sort.by(
+                            Sort.Order.desc("active"),
+                            Sort.Order.asc("name"),
+                            Sort.Order.asc("actualStock"),
+                            Sort.Order.desc("minimalStock")
+                    )
+            );
+        }
+
+        return rawMaterialService.getAll(name, active, RawMaterialUnitOfMeasure.parseFromStringList(unitOfMeasure), lowStock, pageable);
     }
 }
