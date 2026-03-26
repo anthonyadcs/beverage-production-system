35a2ed11b5b4dbaebd2dff407bc4ac562dbd1e31
Anthony <anthonydacsilva10@gmail.com>
2026-03-26 14:19:48 -0300

    feat(recipe): add RecipeRepository with query to find last recipe version

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/RecipeRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/RecipeRepository.java
@@ -1,4 +1,14 @@
 package dev.anthonyadcs.beverage_production_system.repository;
 
-public interface RecipeRepository {
+import dev.anthonyadcs.beverage_production_system.domain.entity.Product;
+import dev.anthonyadcs.beverage_production_system.domain.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
+
+public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
+    //Encontra a última versão da receita por produto. Caso seja a primeira, retorna 0
+    @Query("SELECT COALESCE(MAX(r.version), 0) FROM Recipe r WHERE r.product = :product")
+    Integer findMaxVersionByProduct(Product product);
 }
