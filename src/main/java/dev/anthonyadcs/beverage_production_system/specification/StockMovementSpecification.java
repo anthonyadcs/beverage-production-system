2175dd280df24e315a315d5e278df938bbb12d17
Anthony <anthonydacsilva10@gmail.com>
2026-03-26 00:01:39 -0300

    feat(stock-movement): add StockMovementSpecification

--- src/main/java/dev/anthonyadcs/beverage_production_system/specification/StockMovementSpecification.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/specification/StockMovementSpecification.java
@@ -1,4 +1,27 @@
 package dev.anthonyadcs.beverage_production_system.specification;
 
+import dev.anthonyadcs.beverage_production_system.domain.entity.StockMovement;
+import dev.anthonyadcs.beverage_production_system.domain.enums.StockMovementType;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
 public class StockMovementSpecification {
+    public static Specification<StockMovement> hasRawMaterialId(UUID id) {
+        return (root, query, criteriaBuilder) -> root.get("rawMaterial").get("id").equalTo(id);
+    }
+
+    public static Specification<StockMovement> hasType(List<StockMovementType> stockMovementTypes){
+        return (root, query, criteriaBuilder) -> root.get("type").in(stockMovementTypes);
+    }
+
+    public static Specification<StockMovement> betweenDates(Instant startDateRange, Instant endDateRange){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(
+                root.get("createdAt"),
+                startDateRange,
+                endDateRange
+        );
+    }
 }
