bafd9ffad4f95c7932122b097784066eac49323c
Anthony <anthonydacsilva10@gmail.com>
2026-03-26 00:00:39 -0300

    feat(stock-material/repository): implement JpaSpecificatorExecutor to repository

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/StockMovementRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/StockMovementRepository.java
@@ -2,8 +2,10 @@ package dev.anthonyadcs.beverage_production_system.repository;
 
 import dev.anthonyadcs.beverage_production_system.domain.entity.StockMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface StockMovementRepository extends JpaRepository<StockMovement, UUID> {
+public interface StockMovementRepository extends JpaRepository<StockMovement, UUID>, JpaSpecificationExecutor<StockMovement> {
+
 }
