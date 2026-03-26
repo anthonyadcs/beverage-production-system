7232b412452991e87ad274b9f4c1d1e8b289273e
Anthony <anthonydacsilva10@gmail.com>
2026-03-26 14:23:15 -0300

    feat(recipe/dto): add request DTOs for creating recipes

--- src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/CreateRecipeRequest.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/CreateRecipeRequest.java
@@ -1,4 +1,15 @@
 package dev.anthonyadcs.beverage_production_system.dto.request;
 
-public class CreateRecipeRequest {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record CreateRecipeRequest(
+        @Size(min = 3, max = 500, message = "A descrição da receita deve ter entre 3 e 500 caracteres.")
+        String description,
+
+        @NotNull(message = "É necessário ao menos um insumo para a criação de uma receita.")
+        @Size(min = 1, message = "É necessário ao menos um insumo para a criação de uma receita.")
+        List<RecipeItemRequest> items
+) {}
--- src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/RecipeItemRequest.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/dto/request/RecipeItemRequest.java
@@ -1,4 +1,19 @@
 package dev.anthonyadcs.beverage_production_system.dto.request;
 
-public class RecipeItemRequest {
-}
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record RecipeItemRequest(
+        @NotNull(message = "O código de identificação do insumo é necessário para adicioná-lo como um item de uma receita.")
+        UUID rawMaterialId,
+
+        @NotNull(message = "A quantidade do insumo utilizado para a produção da receita deve ser maior que 0.")
+        @Digits(integer = 10, fraction = 3, message = "A quantidade de insumo utilizado deve ter no máximo 4 casas decimais.")
+        @DecimalMin(value = "0.000", inclusive = false, message = "A quantidade do insumo utilizado para a produção da receita deve ser maior que 0.")
+        BigDecimal quantity
+) {}
