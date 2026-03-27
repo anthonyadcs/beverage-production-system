848c5bc63fe9d51fc126ffa804fb81acc5077a3b
Anthony <anthonydacsilva10@gmail.com>
2026-03-27 00:16:23 -0300

    feat(machine): add machine controller
    expose POST /machines endpoint

--- src/main/java/dev/anthonyadcs/beverage_production_system/controller/MachineController.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/controller/MachineController.java
@@ -1,4 +1,25 @@
 package dev.anthonyadcs.beverage_production_system.controller;
 
+import dev.anthonyadcs.beverage_production_system.dto.request.CreateMachineRequest;
+import dev.anthonyadcs.beverage_production_system.dto.response.MachineResponse;
+import dev.anthonyadcs.beverage_production_system.service.MachineService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/machines")
 public class MachineController {
+    @Autowired
+    private MachineService machineService;
+
+    @PostMapping
+    public ResponseEntity<MachineResponse> create(@RequestBody @Valid CreateMachineRequest machineRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(machineService.create(machineRequest));
+    }
 }
