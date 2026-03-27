29bbb9d7b1e91f43e9f97daa9517e70c2e349906
Anthony <anthonydacsilva10@gmail.com>
2026-03-27 00:13:17 -0300

    feat(machine): add MachineRepository to persist data

--- src/main/java/dev/anthonyadcs/beverage_production_system/repository/MachineRepository.java
+++ src/main/java/dev/anthonyadcs/beverage_production_system/repository/MachineRepository.java
@@ -1,4 +1,11 @@
 package dev.anthonyadcs.beverage_production_system.repository;
 
-public interface MachineRepository {
+import dev.anthonyadcs.beverage_production_system.domain.entity.Machine;
+import dev.anthonyadcs.beverage_production_system.domain.valueObject.EntityCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MachineRepository extends JpaRepository<Machine, UUID> {
+    boolean existsByCode(EntityCode code);
 }
